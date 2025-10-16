8d4e3da6a9ab015578128b6ff577fc04bd14d44b
Harrisong Gutierrez <harrisong.gutierrez@smbssolutions.com>
2025-10-16 10:40:42 -0600

    fixes

--- public/main.js
+++ public/main.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f5425986a92493c5955da485120c14b8bba3ac8a3b0f3e7a06de3bd95aa8ecf9
-size 8382
+oid sha256:ffeeac831acadf03e13db70cab42bbfa35e0f7028434ba435d9cc1757818660b
+size 4516
