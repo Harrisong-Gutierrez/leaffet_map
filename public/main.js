5ad0c31de75e8753b31797234afd1560d3806e0f
Harrisong Gutierrez <harrisong.gutierrez@smbssolutions.com>
2025-10-20 11:49:51 -0600

    change colors

--- public/main.js
+++ public/main.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1a51c787dee09a4837b8f84b2604cf4e82110083e560840521612ab11ba858d9
-size 3957
+oid sha256:bf83523f7bd70cb0230308e495b689b5b196bbdc2a00c8b019c69c53e1d6db6c
+size 5866
