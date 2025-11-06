28b679846c9c923043713c6478a9e1e640ab411e
Harrisong Gutierrez <harrisong.gutierrez@smbssolutions.com>
2025-11-06 11:06:53 -0600

    feature colors

--- public/main.js
+++ public/main.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4e04241b6834541d420abfcc853d0d413f67e589fdb78be0e4cd6bff7bd57033
-size 8428
+oid sha256:3d8486d14cfc63bacb12e61e66c1b425e153eac46d2d5398a2c0a3d885fd8d9a
+size 9125
