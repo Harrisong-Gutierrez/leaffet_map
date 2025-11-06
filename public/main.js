aba3f80d9b21808d7c6fcffd4238039f416982f5
Harrisong Gutierrez <harrisong.gutierrez@smbssolutions.com>
2025-11-06 09:39:57 -0600

    Distrito 7 done

--- public/main.js
+++ public/main.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e0b6b01195affccab2ab7d4c572f5239f04f9d31f04a63769f909adcc4bada5b
-size 13676
+oid sha256:4e04241b6834541d420abfcc853d0d413f67e589fdb78be0e4cd6bff7bd57033
+size 8428
