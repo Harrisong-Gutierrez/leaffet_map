1e0b07461f02a2267e09dd47504eab05ddc1d569
Harrisong Gutierrez <harrisong.gutierrez@smbssolutions.com>
2025-11-07 12:01:58 -0600

    change geo data structure

--- public/main.js
+++ public/main.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ceb35c41ddcb81cae34499db434c597ba787727b37e56803a7de9c5ea95ed57e
-size 7954
+oid sha256:63b2a582558d153536ff3a372e7571caa717f860c2880ca689b48040379f8049
+size 9521
