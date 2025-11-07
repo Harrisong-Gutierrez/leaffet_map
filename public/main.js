5fa8e10c3b68b36d4daf2164f0694adc18c0b98c
Harrisong Gutierrez <harrisong.gutierrez@smbssolutions.com>
2025-11-07 14:08:34 -0600

    departamentos

--- public/main.js
+++ public/main.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:63b2a582558d153536ff3a372e7571caa717f860c2880ca689b48040379f8049
-size 9521
+oid sha256:58189cb40682de7357f7fa12c952b09ede82cf973b12ffe7bae155b16cbf03bc
+size 9514
--- server.js
+++ server.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d48f3dfc822d77f35fd73ddd91554b1046d55a542e15a9d78b599a419b5d6ba6
-size 1624
+oid sha256:89695a5a663b7535ad23cf6460dfd1dd872d387349c7dff3d634230b64f7d2c2
+size 1603
