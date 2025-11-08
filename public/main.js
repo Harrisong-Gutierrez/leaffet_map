63acf3f9600fe5713a3c4b670490aa8ba2237a52
Harrisong Gutierrez <harrisong.gutierrez@smbssolutions.com>
2025-11-07 21:23:41 -0600

    render problem

--- public/main.js
+++ public/main.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:58189cb40682de7357f7fa12c952b09ede82cf973b12ffe7bae155b16cbf03bc
-size 9514
+oid sha256:139cf707dcf4237788af00c9638e45f7b4713c943f4dae079a50730878eaebba
+size 9561
