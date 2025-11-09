963bc7dcc00a93809e65560e934f4c4ead45b800
Harrisong Gutierrez <harrisong.gutierrez@smbssolutions.com>
2025-11-09 13:15:19 -0600

    developing preformance

--- public/main.js
+++ public/main.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:139cf707dcf4237788af00c9638e45f7b4713c943f4dae079a50730878eaebba
-size 9561
+oid sha256:2e2f9782fdd161587efa8a07a79eb657bdb963556b18ec4fceea3f6ecd52d1e4
+size 12598
--- server.js
+++ server.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:89695a5a663b7535ad23cf6460dfd1dd872d387349c7dff3d634230b64f7d2c2
-size 1603
+oid sha256:9430709986b80df757c5370dedf7515d92ede9433c82a08b7326a021f0753efb
+size 3426
