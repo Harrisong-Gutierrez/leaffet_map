e4958af17dacfc925413f546448ae759125d4775
Harrisong Gutierrez <harrisong.gutierrez@smbssolutions.com>
2025-11-09 17:15:36 -0600

    Track and add barrios.geojson with Git LFS

--- public/main.js
+++ public/main.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5f5145eb7dbe36a80f5c1fac44d88d2c418d72b7b866cb8a03df67286c3e0563
-size 14353
+oid sha256:05c1fb96a5a489f68f262092e5fa9327c6c7db8fb5f7fe8a0743c160fc0f3a27
+size 6694
--- server.js
+++ server.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:90a9a9e386e1d39a354cc765853cc17af6651d1fa2b0dea9c87d3b035d8d24d8
-size 5498
+oid sha256:a092f10c5cfbb2cb7270444f98acff8f20d543cf7d334a0e5425cdd09490d2dd
+size 669
