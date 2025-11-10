52566ad582d47df9b8b4977886d0fa01436f2479
Harrisong Gutierrez <harrisong.gutierrez@smbssolutions.com>
2025-11-10 14:27:57 -0600

    developing preformance

--- server.js
+++ server.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a092f10c5cfbb2cb7270444f98acff8f20d543cf7d334a0e5425cdd09490d2dd
-size 669
+oid sha256:de2d9aad80360a3aa87c224acb7f2cdff8f04ad2355cfc3c4455a02cd2fdb0d6
+size 556
