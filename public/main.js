a3eaf827d7cdf6878567a08b4fa20e38f1bccbf3
Harrisong Gutierrez <harrisong.gutierrez@smbssolutions.com>
2025-11-12 11:20:33 -0600

    nombres para los poligonos ...

--- public/main.js
+++ public/main.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f576b00a3a27ed1e4a55e5b02cf94d4f3179736ec8c92daf9adb7f8aa4b552b2
-size 7889
+oid sha256:04ac3736d8c5ac4b0c8356f0b3eb12a8806860355047990df3c90cdfdaab5189
+size 8838
