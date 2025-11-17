95a0b9dd70d71e0d9e1423f49e51360615468b4d
Harrisong Gutierrez <harrisong.gutierrez@smbssolutions.com>
2025-11-17 14:47:27 -0600

    filter prototype

--- public/main.js
+++ public/main.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f7ac236bd32e86073661f1668bcb258714ec5760e194760a81d1e3706fac6de0
-size 10582
+oid sha256:e6057c7fb2c10d19e1815a975cbe620b1a07eb474fd4c869108236eac39a99de
+size 11188
